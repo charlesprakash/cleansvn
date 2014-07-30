bd41db5136cad5056cd12aa624c6792cde2eb0e9
charlesprakash <prakashd@schakra.com>
2014-07-30 00:37:36 -0700

    A simple tool to clean SVN stale files

---
+++ CleanSvn/CleanSvn/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanSvn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootFolder = string.Empty;
+
+            if (args.Length == 0)
+            {
+                Console.Write("Root folder: ");
+                rootFolder = Console.ReadLine();
+            }
+            else
+            {
+                rootFolder = args[0];
+            }
+
+            rootFolder = string.IsNullOrEmpty(rootFolder) ? string.Empty : rootFolder.Trim();
+
+            if (!Directory.Exists(rootFolder))
+            {
+                Console.WriteLine("Folder {0} doesn't exist", rootFolder);
+                return;
+            }
+
+            RemoveSvnFolder(rootFolder);
+        }
+
+        static void RemoveSvnFolder(string parentFolder)
+        {
+            string svnFolder = Path.Combine(parentFolder, ".svn");
+            if (Directory.Exists(svnFolder))
+            {
+                Console.WriteLine(svnFolder);
+                ForceDeleteFolder(svnFolder);
+            }
+
+            foreach (var child in Directory.GetDirectories(parentFolder))
+            {
+                RemoveSvnFolder(child);
+            }
+        }
+
+        static void ForceDeleteFolder(string svnFolder)
+        {
+            foreach (string file in Directory.GetFiles(svnFolder, "*", SearchOption.AllDirectories))
+            {
+                var deleted = DeleteFile(@"\\?\" + file);
+                Console.WriteLine("{0} - {1}", file, deleted);
+            }
+            Directory.Delete(svnFolder, true);
+        }
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        static extern bool DeleteFile(string lpszLongPath);
+    }
+}
